14378b2eb5382a11791a47756c27a9184a81ce72
Bouzid Krita <bouzid.krita@hotmail.fr>
2023-12-30 16:42:53 +0100

    feat: update package.json and add .env file

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,19 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"
 import { getDatabase } from "firebase/database"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCQNKaXTtBypXcQO85tSVn7q7_h--OYBAk",
-  authDomain: "electricvuechat.firebaseapp.com",
-  databaseURL: "https://electricvuechat-default-rtdb.firebaseio.com",
-  projectId: "electricvuechat",
-  storageBucket: "electricvuechat.appspot.com",
-  messagingSenderId: "304249265542",
-  appId: "1:304249265542:web:04e7ecc95f65618c60a007"
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_DATABASE_URL,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig)
